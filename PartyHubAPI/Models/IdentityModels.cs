3691e29c166fa409f6d636562f23e25dfc1f3794
Derek Elum <Derek Elum>
2017-02-23 21:18:14 -0600

    updated the user still working on Login

--- PartyHubAPI/Models/IdentityModels.cs
+++ PartyHubAPI/Models/IdentityModels.cs
@@ -3,12 +3,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartyHubAPI.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        public string FirstName { get; set; }
+
+        
+        public string  LastName { get; set; }
+
+        public string  NickName { get; set; }
+
+
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string  DOB { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
