30d82d61ef673bbcb8648cb1e0f7c211a0bee4ed
Derek Elum <Derek Elum>
2017-02-23 19:58:51 -0600

    initial commit

---
+++ PartyHubAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PartyHubAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ PartyHubAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PartyHubAPI.WebApiApplication" Language="C#" %>
